a2d9b1691f860a4d2fd0ad230b4ef7ff6002653a
afifsohaili <mafif@booster.com>
2014-11-08 08:43:01 +0800

    Called initialize of parent

--- lib/cosi.rb
+++ lib/cosi.rb
@@ -1,5 +1,8 @@
 class Cosi < Restaurant
-
+  def initialize
+    super
+  end
+  
   def start
     puts "Cosi opened"
     # (@seconds).times do |second|
--- lib/mcd.rb
+++ lib/mcd.rb
@@ -1,5 +1,8 @@
 class McDonalds < Restaurant
-
+  def initialize
+    super
+  end
+  
   def start
     puts "McDonalds opened"
     # (@seconds).times do |second|
